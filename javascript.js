f130d5dda962c80a8821a8c48469ef4c4c188fe2
Tails99 <22770251+Tails99@users.noreply.github.com>
2024-07-01 23:09:07 -0300

    Added info messages between rounds and tie messages

--- javascript.js
+++ javascript.js
@@ -22,17 +22,25 @@ let robot = 0;
 
 function playRound(humanChoice, computerChoice) {
     if(humanChoice=='paper' && computerChoice =='rock'){
+        console.log('You won, robot picked rock');
         human++;
     }else if(humanChoice=='paper' && computerChoice=='scissor'){
+        console.log('You lost, robot picked scissor');
         robot++;
     }else if(humanChoice=='rock' && computerChoice=='paper'){
+        console.log('You lost, robot picked paper');
         robot++;
     }else if(humanChoice=='rock' && computerChoice=='scissor'){
+        console.log('You won, robot picked scissor');
         human++;
     }else if(humanChoice=='scissor' && computerChoice=='paper'){
+        console.log('You won, robot picked paper');
         human++;
     }else if(humanChoice=='scissor' && computerChoice=='rock'){
+        console.log('You lost, robot picked rock');
         robot++;
+    }else{
+        console.log('Tie')
     }
 
 }
