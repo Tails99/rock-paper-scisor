740b676df282ddec5ca1bb3b9ed46cb7da4656e8
Tails99 <22770251+Tails99@users.noreply.github.com>
2024-07-01 23:04:02 -0300

    Added rounds and game functions

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-function getComputerChoise(){
+function getComputerChoice(){
     let bot = getRandomInt(3);
     if(bot == 0){
         return 'paper';
@@ -15,5 +15,41 @@ function getComputerChoise(){
 
 function getHumanChoice(){
     let choice = prompt('Choose your move (rock/paper/scissor)');
-    return choice;
+    return choice.toLowerCase();
 }
+let human = 0;
+let robot = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice=='paper' && computerChoice =='rock'){
+        human++;
+    }else if(humanChoice=='paper' && computerChoice=='scissor'){
+        robot++;
+    }else if(humanChoice=='rock' && computerChoice=='paper'){
+        robot++;
+    }else if(humanChoice=='rock' && computerChoice=='scissor'){
+        human++;
+    }else if(humanChoice=='scissor' && computerChoice=='paper'){
+        human++;
+    }else if(humanChoice=='scissor' && computerChoice=='rock'){
+        robot++;
+    }
+
+}
+
+function playgame(){
+    for (let i = 0; i < 5; i++){
+        let person=getHumanChoice();
+        let game=getComputerChoice();
+        playRound(person,game);
+    }
+    if(robot>human){
+       console.log('Computer wins, score:' + robot);
+    }else if(robot<human){
+      console.log('You win: score:' + human);
+    }else{
+        console.log('Tie');
+    }
+}
+
+playgame();
